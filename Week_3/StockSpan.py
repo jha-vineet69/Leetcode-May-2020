4da527a79fa1198109d5eefd26792059ae392298
Vineet Jha <vineetjha3388@gmail.com>
2020-05-19 15:44:32 +0530

    Solved Question 19

---
+++ Week_3/StockSpan.py
@@ -0,0 +1,24 @@
+class StockSpanner:
+
+    def __init__(self):
+        self.st = []
+        self.d = 0
+        
+
+    def next(self, price: int) -> int:
+        l =-1
+        while self.st and self.st[-1][0]<=price:
+            self.st.pop()
+        if self.st:
+            l = self.st[-1][-1]
+        ans = self.d-l
+        self.st.append((price, self.d))
+        self.d+=1
+        
+        return ans
+        
+
+
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)
